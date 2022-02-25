2022394be535552103d72ac1b8e4f486418b24cc
akhileshpandey <akhilespande@gmail.com>
2022-02-25 16:47:55 +0530

    jdbc batch operation

--- src/com/akhilesh/programs/jdbcprograms/DbConstants.java
+++ src/com/akhilesh/programs/jdbcprograms/DbConstants.java
@@ -5,7 +5,9 @@ public class DbConstants {
     static final String USER_ID = "root";
     static final String PASSWORD = "root";
     static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-
     static final String SELECT_QUERY = "select articleid , title , category from article";
-
+    static final String INSERT_QUERY = "INSERT INTO Article Values(3,'Election' , 'UP election')";
+    static final String DELETE_QUERY = "DELETE FROM Article WHERE articleid = 1";
+    static final String UPDATE_QUERY = "UPDATE Article SET category = 'Election' WHERE articleId = 2";
+    static final String INSERT_QUERY_FOR_PREPARED_STATEMENT =  "INSERT INTO Article Values(?,?,?)";;
 }
